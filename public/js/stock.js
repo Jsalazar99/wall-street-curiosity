1fc49cac12a3e42275d4c2f700265abdb8e7f57c
chelseyvalerio <cvalerio@csog.net>
2023-04-10 21:22:52 -0600

    updated js and handlebars

--- public/js/stock.js
+++ public/js/stock.js
@@ -5,28 +5,52 @@ if (!data.feed) {
 } else {
     search.innerHTML = "";
 
+
+    // POST for addStock btn event listener.  
+    if (name && needed_funding && description) {
+        const response = await fetch(`/api/projects`, {
+          method: 'POST',
+          body: JSON.stringify({ name, needed_funding, description }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+    
+        if (response.ok) {
+          document.location.replace('/profile');
+        } else {
+          alert('Failed to create project');
+        }
+      }
+    };
+
+    document
+  .querySelector('.new-project-form')
+  .addEventListener('submit', newFormHandler);
+    
+
     // change the variable names here for stock handlebars     
-    var articleEl = document.querySelector(".current-news");
-    articleEl.textContent = ''
-    for (var i = 0; i < 50; i++) {
-        var articleData = data.feed[i];
+    // var articleEl = document.querySelector(".current-news");
+    // articleEl.textContent = ''
+    // for (var i = 0; i < 50; i++) {
+    //     var articleData = data.feed[i];
 
-        var cardBody = document.createElement("div");
-        var articletitle = document.createElement("h5");
-        var articleSummary = document.createElement("p");
-        var linkButtonEl = document.createElement("a");
+    //     var cardBody = document.createElement("div");
+    //     var articletitle = document.createElement("h5");
+    //     var articleSummary = document.createElement("p");
+    //     var linkButtonEl = document.createElement("a");
 
-        cardBody.classList.add('card');
-        articleSummary.textContent = 'Summary: ' + articleData.summary
-        articletitle.textContent = 'Title: ' + articleData.title
-        linkButtonEl.textContent = 'Read More';
-        linkButtonEl.setAttribute('href', articleData.url);
-        linkButtonEl.setAttribute('target', '_blank');
-        linkButtonEl.classList.add('btn');
+    //     cardBody.classList.add('card');
+    //     articleSummary.textContent = 'Summary: ' + articleData.summary
+    //     articletitle.textContent = 'Title: ' + articleData.title
+    //     linkButtonEl.textContent = 'Read More';
+    //     linkButtonEl.setAttribute('href', articleData.url);
+    //     linkButtonEl.setAttribute('target', '_blank');
+    //     linkButtonEl.classList.add('btn');
 
-        articleEl.appendChild(cardBody);
-        cardBody.appendChild(articletitle);
-        cardBody.appendChild(articleSummary);
-        cardBody.appendChild(linkButtonEl);
-    }
-};
+    //     articleEl.appendChild(cardBody);
+    //     cardBody.appendChild(articletitle);
+    //     cardBody.appendChild(articleSummary);
+    //     cardBody.appendChild(linkButtonEl);
+    // }
+
