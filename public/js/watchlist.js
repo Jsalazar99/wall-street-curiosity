45bd83e8c1c4a468923e6027c732d439e9a85dff
Jesus S <jsalazar2010@gmail.com>
2023-04-11 18:31:27 -0600

    moved delete method to watchlist.js from stock.js

--- public/js/watchlist.js
+++ public/js/watchlist.js
@@ -1,19 +1,21 @@
-const newFormHandler = async (event) => {
+/*const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#project-name').value.trim();
   const needed_funding = document.querySelector('#project-funding').value.trim();
   const description = document.querySelector('#project-desc').value.trim();
 }
+*/
 
-
-const delButtonHandler = async (event) => {
+// DELETE method for removing stocks from watchlist
+const deleteButton = async (event) => {
   if (event.target.hasAttribute('data-id')) {
     const id = event.target.getAttribute('data-id');
 
     const response = await fetch(`/api/stocks/${id}`, {
       method: 'DELETE',
     });
+    console.log(response);
 
     if (response.ok) {
       document.location.replace('/watchlist');
@@ -23,7 +25,7 @@ const delButtonHandler = async (event) => {
   }
 };
 
-
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+document.querySelectorAll('[data-id]')
+  .forEach(element => {
+    element.addEventListener('click', deleteButton);
+  });
